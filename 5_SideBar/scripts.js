7e1c49c77539959f6d52c16dab63e3de9c79f92d
yadav-deepakk <deepakyadav53882@gmail.com>
2023-03-07 13:00:09 +0530

    changed side-bar's default state to display:none

--- 5_SideBar/scripts.js
+++ 5_SideBar/scripts.js
@@ -1,3 +1,18 @@
 document.querySelector(".close-side-bar").addEventListener("click", () => sideBarToggler("none"));
 document.querySelector(".hamburger-menu").addEventListener("click", () => sideBarToggler("block"));
 const sideBarToggler = (displayProp) => document.querySelector(".side-bar-bg").style.display = displayProp;
+
+function makeActiveLink(domElement){
+	console.log(domElement.classList);
+	if(domElement.classList.contains("active")) return;
+	else{
+		let children = domElement.parentNode.children;
+		console.log(children);
+		for (var i=children.length -1; i>=0; i--) {
+			if ( children[i].classList.contains("active") ){
+				children[i].classList.remove("active");
+			}
+		}
+		domElement.classList.add("active");
+	}
+}
