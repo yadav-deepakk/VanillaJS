7bd5914c2069e82b2234d3443ce898e15f41016e
yadav-deepakk <deepakyadav53882@gmail.com>
2023-03-16 22:30:24 +0530

    added click listener to indicator, indicators can change active slide as they clicked.

--- 15_Slider/scripts.js
+++ 15_Slider/scripts.js
@@ -6,29 +6,31 @@ let prevBtn = document.querySelector('#prev-btn');
 let nextBtn = document.querySelector('#next-btn');
 
 slides.forEach((slide,index) =>	slide.style.left = `${index * 100}%`);
+indicators.forEach((indicator,index) => {
+	indicator.addEventListener('click', () => moveSlide(direction=null, indicatorIndex=index));
+});
 
-function moveSlide(direction){
+function moveSlide(direction=null, indicatorIndex=null){
+	if(direction!==null)
+		direction.toLowerCase()==='left' ? activeSlide-=1:activeSlide+=1;
+	else activeSlide = indicatorIndex;
 
-	direction.toLowerCase()==='left' ? activeSlide-=1 : activeSlide+=1;
+	if(activeSlide>=indicators.length) activeSlide=0;
+	else if(activeSlide<0) activeSlide=indicators.length-1;
 
-	if(activeSlide<0) 
-		activeSlide = slides.length -1;
-	else if (activeSlide == slides.length)
-		activeSlide = 0;
+	// console.log(`Direction:${direction}, IndicatorIndex:${indicatorIndex}, ActiveSlide:${activeSlide +1}`);
 
-	slides.forEach((slide,index) => {
-		slide.style.transform = `translate(-${activeSlide * 100}%, 0px)`;
+	slides.forEach((slide, index) => {
+		slide.style.transform = `translateX(-${activeSlide * 100}%)`
 	});
-
-	indicators.forEach((indicator,index) => {
-		if(index==activeSlide) 
+	indicators.forEach((indicator, index) => {
+		if(index===activeSlide){
 			indicator.classList.add('active-slide');
-		else if(index!==activeSlide && indicator.classList.contains('active-slide')) 
+		}else{
 			indicator.classList.remove('active-slide');
+		}
 	});
-
-	console.log(`active slide is: ${activeSlide +1}`)
 }
 
-prevBtn.addEventListener('click', () => moveSlide('left'));
-nextBtn.addEventListener('click', () => moveSlide('right'));
+prevBtn.addEventListener('click', () => moveSlide(direction='left', indicatorIndex=null));
+nextBtn.addEventListener('click', () => moveSlide(direction='right', indicatorIndex=null));
